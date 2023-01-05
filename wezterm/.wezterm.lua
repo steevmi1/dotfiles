fb23c45483892eba3f3358d4e3440cf854e79b0c
Michael Steeves <michael@steeves.io>
2023-01-04 19:49:04 -0500

    Initial check-in for wezterm config.

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font('Inconsolata Nerd Font'),
+  font_size = 16.0,
+  color_scheme = "Gruvbox dark, hard (base16)",
+  initial_rows = 45,
+  initial_cols = 120,
+  window_padding = {
+    left = 3,
+    right = 3,
+    top = 3,
+    bottom = 3,
+  },
+}
